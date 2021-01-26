56298d60bbabee273396f0e358846303351996a4
lrennels <lrennels@berkeley.edu>
2021-01-26 12:07:40 -0800

    Remove getdicegams code since it was duplicative

--- test/runtests.jl
+++ test/runtests.jl
@@ -77,57 +77,9 @@ end #MimiDICE2016R2-excel-model testset
 #------------------------------------------------------------------------------
 @testset "MimiDICE2016R2-gams-model" begin
 
-m = MimiDICE2016R2.getdicegams()
-run(m)
-
 gams_results = CSVFiles.load(joinpath(@__DIR__, "../data/DICE2016R2-GAMS-Results-select.csv")) |> DataFrame
 
 Precision = 1.0e-10
-
-#TATM Test (temperature increase)
-True_TATM = gams_results[:TATM];
-@test maximum(abs, m[:climatedynamics, :TATM] .- True_TATM) ≈ 0. atol = Precision
-
-#MAT Test (carbon concentration atmosphere)
-True_MAT = gams_results[:MAT];
-@test maximum(abs, m[:co2cycle, :MAT] .- True_MAT) ≈ 0. atol = Precision
-
-#DAMFRAC Test (damages fraction)
-True_DAMFRAC = gams_results[:DAMFRAC];
-@test maximum(abs, m[:damages, :DAMFRAC] .- True_DAMFRAC) ≈ 0. atol = Precision
-
-#DAMAGES Test (damages $)
-True_DAMAGES = gams_results[:DAMAGES];
-@test maximum(abs, m[:damages, :DAMAGES] .- True_DAMAGES) ≈ 0. atol = Precision
-
-#E Test (emissions)
-True_E = gams_results[:E];
-@test maximum(abs, m[:emissions, :E] .- True_E) ≈ 0. atol = Precision
-
-#YGROSS Test (gross output)
-True_YGROSS = gams_results[:YGROSS];
-@test maximum(abs, m[:grosseconomy, :YGROSS] .- True_YGROSS) ≈ 0. atol = Precision
-
-# don't have the data for this one
-# #AL test (total factor productivity)
-# True_AL = gams_results[:AL]
-# @test maximum(abs, m[:totalfactorproductivity, :AL] .- True_AL) ≈ 0. atol = Precision
-
-#CPC Test (per capita consumption)
-True_CPC = gams_results[:CPC];
-@test maximum(abs, m[:neteconomy, :CPC] .- True_CPC) ≈ 0. atol = Precision
-
-#FORCOTH Test (exogenous forcing)
-True_FORCOTH = gams_results[:FORCOTH];
-@test maximum(abs, m[:radiativeforcing, :FORCOTH] .- True_FORCOTH) ≈ 0. atol = Precision
-
-#FORC Test (radiative forcing)
-True_FORCOTH = gams_results[:FORC];
-@test maximum(abs, m[:radiativeforcing, :FORC] .- True_FORC) ≈ 0. atol = Precision
-
-#Utility Test
-True_UTILITY = gams_results[:UTILITY][1];
-@test maximum(abs, m[:welfare, :UTILITY] .- True_UTILITY) ≈ 0. atol = Precision
 
 end #MimiDICE2016R2-gams-model testset
 
